8def209f2cd486b48b182c39254574e2256b69da
rokas <rokasra@gmail.com>
2022-06-16 17:28:37 +0300

    viewlog empty

--- src/pages/ViewLogsPage.js
+++ src/pages/ViewLogsPage.js
@@ -1,34 +1,42 @@
-import { useEffect, useState } from 'react';
-// import SingleLogPage from '../components/ViewLogs/SingleLogPage';
-import ViewLogs from '../components/ViewLogs/ViewLogs';
+// import { useEffect, useState } from 'react';
+// // import SingleLogPage from '../components/ViewLogs/SingleLogPage';
+// import ViewLogs from '../components/ViewLogs/ViewLogs';
 
-const ViewLogsPage = () => {
-  const [petsArr, setPetsArr] = useState([]);
-  const getPetsAndSetState = async () => {
-    const resp = await fetch('https://glittery-dull-snickerdoodle.glitch.me/v1/pets/');
-    const dataInJs = await resp.json();
+// const ViewLogsPage = () => {
+//   const [petsArr, setPetsArr] = useState([]);
+//   const getPetsAndSetState = async () => {
+//     const resp = await fetch('https://glittery-dull-snickerdoodle.glitch.me/v1/pets/');
+//     const dataInJs = await resp.json();
 
-    const onlyName = dataInJs.map(({ name }) => ({
-      name,
-    }));
-    console.log('onlyName===', onlyName);
-    setPetsArr(onlyName);
-  };
+//     const onlyName = dataInJs.map(({ name }) => ({
+//       name,
+//     }));
+//     console.log('onlyName===', onlyName);
+//     setPetsArr(onlyName);
+//   };
 
-  useEffect(() => {
-    getPetsAndSetState();
-  });
+//   useEffect(() => {
+//     getPetsAndSetState();
+//   });
 
-  return (
-    <div>
-      {petsArr.map((pObj) => (
-        <div>
-          {/* <SingleLogPage key={pObj.id} {...pObj} /> */}
-          <ViewLogs key={pObj.id} {...pObj} />
-        </div>
-      ))}
-    </div>
-  );
+//   return (
+//     <div>
+//       {petsArr.map((pObj) => (
+//         <div>
+//           {/* <SingleLogPage key={pObj.id} {...pObj} /> */}
+//           <ViewLogs key={pObj.id} {...pObj} />
+//         </div>
+//       ))}
+//     </div>
+//   );
+// };
+
+// export default ViewLogsPage;
+
+//__________________________________________________//
+
+const ViewLogs = (props) => {
+  return <div>ViewLogsPage{props.id}</div>;
 };
 
-export default ViewLogsPage;
+export default ViewLogs;
